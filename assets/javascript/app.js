2433434937a91aa1126783b394eba5f65a29b426
Doyle <patrick.doyle@stryker.com>
2018-01-26 14:25:42 -0600

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+
+
+$("#contactButton").on("click", function(){
+	$("#yourName").val("");
+	$("#telephone").val("");
+	$("#exampleInputEmail").val("");
+	$("#telephone").val("");
+	$("#description").val("");
+})
